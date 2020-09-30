9cbb52a2326309fe5d74756c597c62a2f49c6395
mei <jgtop1010@gmail.com>
2020-09-30 11:35:41 +0000

    ログアウト機能実装 #5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
   
   private
