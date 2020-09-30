b7dbae446d1cef7b0c45333cf4f685add9bedd9e
mei <jgtop1010@gmail.com>
2020-09-30 06:52:31 +0000

    createアクション #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,20 @@ class UsersController < ApplicationController
   end
 
   def create
+     @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = '登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = '登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
