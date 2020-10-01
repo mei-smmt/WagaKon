6928e298705bfc61c26887bb174e5c43d2f0f83f
mei <jgtop1010@gmail.com>
2020-10-01 01:59:44 +0000

    コントローラ 設定 #7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,33 @@ class UsersController < ApplicationController
      @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = '登録しました。'
+      flash[:success] = '登録しました'
       redirect_to root_url
     else
-      flash.now[:danger] = '登録に失敗しました。'
+      flash.now[:danger] = '内容に誤りがあります'
       render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = 'ユーザー情報が更新されました'
+      redirect_to @user
+    else
+      flash.now[:danger] = '内容に誤りがあります'
+      render :edit
     end
   end
   
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :password, :password_confirmation)
   end
 end
