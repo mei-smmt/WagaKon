86a7b37c19b84e68342038f587c4404a40d46a2f
mei <jgtop1010@gmail.com>
2020-10-01 04:34:26 +0000

    imageカラムの追加 #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_053544) do
+ActiveRecord::Schema.define(version: 2020_10_01_043234) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_053544) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
