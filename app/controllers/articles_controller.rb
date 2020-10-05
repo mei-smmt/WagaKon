f4ecc5019d9953e190bc78308b1913067fbce2d4
mei <jgtop1010@gmail.com>
2020-10-05 09:37:46 +0000

    articlesコントローラ設定、モデル修正 #10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,28 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    if logged_in?
+      @article = current_user.articles.build
+    end
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to new_material_url
+    else
+      flash.now[:danger] = '内容に誤りがあります'
+      render :new
+    end
   end
 
   def edit
   end
+  
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :image, :explanation)
+  end
+  
 end
