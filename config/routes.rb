2ffcacaee65bd24cbbf76fe7b085ec67278cbc5d
mei <jgtop1010@gmail.com>
2020-10-05 06:21:56 +0000

    articlesコントローラ 作成 #10

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create, :edit, :update]
   
-  resources :articles
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :materials, only: [:new, :create, :edit, :update, :destroy]
   resources :steps, only: [:new, :create, :edit, :update, :destroy]
 end
