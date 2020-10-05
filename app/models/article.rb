04ff08673bc687f910a2aa69171ece57513352ad
mei <jgtop1010@gmail.com>
2020-10-05 05:00:19 +0000

    モデルの設定 #10

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :materials, :steps
+  
+  # タイトル、画像、説明文必須
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :image, presence: true
+  validates :explanation, presence: true, length: { maximum: 400 }
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,7 @@
 class Step < ApplicationRecord
   belongs_to :article
+  
+  # 手順番号、手順説明文必須
+  validates :number, presence: true
+  validates :content, presence: true, length: { maximum: 400 }
 end
