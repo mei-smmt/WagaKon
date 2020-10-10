e9797bd36739c4b8a85bb8116cda2ebc8b2b8685
mei <jgtop1010@gmail.com>
2020-10-10 01:46:08 +0000

    複数投稿化（要修正） #10

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,13 +2,29 @@ class MaterialsController < ApplicationController
   
   def new
     @article = Article.find(params[:id])
-    @material = @article.materials.build
+    @materials = (1..10).map do
+      @article.materials.build
+    end
   end
   
   def create
     @article = Article.find(params[:article_id])
-    @material = @article.materials.build(material_params)
-    if @material.save
+    
+    @materials = (1..10).map do
+      @article.materials.build
+    end
+    
+    materials_require.each_with_index do |material_require, index|
+      material_params = material_require.permit(:name, :quantity)
+      @materials[index] = @article.materials.new(material_params)
+    end
+
+    is_success = true
+    @materials.each do |material|
+      is_success = false unless material.save
+    end
+      
+    if is_success == true
       redirect_to root_url
     else
       flash.now[:danger] = '内容に誤りがあります'
@@ -17,9 +33,14 @@ class MaterialsController < ApplicationController
   end
   
     private
-
-  def material_params
-    params.require(:material).permit(:name, :quantity)
+    
+  def materials_require
+    params.require(:materials)
   end
 
 end
+
+# materials_params -> requireを消す
+# save失敗したときの処理　transaction, rescue
+# 空の配列定義
+# 要素の追加　<<
