75d3635614389bf0614182fc284b475fea871a51
mei <jgtop1010@gmail.com>
2020-10-10 10:28:32 +0000

    dependent::destroy #10

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   belongs_to :user
-  has_many :materials
-  has_many :steps
+  has_many :materials, dependent: :destroy
+  has_many :steps, dependent: :destroy
   
   # タイトル、画像、説明文必須
   validates :title, presence: true, length: { maximum: 20 }
