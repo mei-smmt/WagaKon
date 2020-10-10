2d8597af56dcc31c6a369a8b59f1a377de72ca2b
mei <jgtop1010@gmail.com>
2020-10-10 08:01:34 +0000

    複数step一括保存機能 #10

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -6,7 +6,31 @@ class StepsController < ApplicationController
       @article.steps.build
     end
   end
+  
+  def create
+    # binding.pry
+    @article = Article.find(params[:article_id])
+   
+    @steps = []
+    steps_params["steps"].each do |step|
+      @steps << @article.steps.build(step)
+    end
+    
+    if Step.bulk_create(@steps)
+      redirect_to root_url
+    else
+      flash.now[:danger] = '内容に誤りがあります'
+      render :new
+    end
+  end
 
   def edit
   end
+  
+  private
+  
+  def steps_params
+    params.permit(steps: [:number, :image, :content])
+  end
+
 end
