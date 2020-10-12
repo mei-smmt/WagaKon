e645d9ce48a7ec072fee265477b2056625b24a61
mei <jgtop1010@gmail.com>
2020-10-12 10:19:34 +0000

    ルーティング #21

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
   resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
 
   get "/:id/materials/new", to: "materials#new"
-  resources :materials, only: [:create, :edit, :update, :destroy]
+  get "/:id/materials/edit", to: "materials#edit"
+  resources :materials, only: [:create, :update]
   
   get "/:id/steps/new", to: "steps#new"
-  resources :steps, only: [:create, :edit, :update, :destroy]
+  get "/:id/steps/edit", to: "stepls#edit"
+  resources :steps, only: [:create, :update]
 end
