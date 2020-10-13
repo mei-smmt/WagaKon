0649aeb4c81fcc0c351c86d21206e37670aa1873
mei <jgtop1010@gmail.com>
2020-10-13 11:23:48 +0000

    ブックマークコントローラ設定 #25

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,13 +2,13 @@ class BookmarksController < ApplicationController
 before_action :require_user_logged_in
 
   def create
-    article = Article.find(params[:id])
+    article = Article.find(params[:article_id])
     current_user.bookmark(article)
     redirect_to article
   end
 
   def destroy
-    article = Article.find(params[:id])
+    article = Article.find(params[:article_id])
     current_user.unbookmark(article)
     redirect_to article
   end
