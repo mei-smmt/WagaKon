db03847fcd6a7735e08dd9c44f34b41e9f8aed77
mei <jgtop1010@gmail.com>
2020-10-30 12:53:08 +0000

    usersコントローラ #favorite_articlesテスト記述 #43

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -102,4 +102,97 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+  
+  describe 'Patch #update' do
+    context '被編集ユーザーとログインユーザーが一致' do
+      before do
+        @user = create(:user)
+        @originalname = @user.name
+        session[:user_id] = @user.id
+      end
+      context '有効なパラメータの場合' do
+        before do
+          patch :update, params:{id: @user.id, user: attributes_for(:user, name: "new_name")}
+        end
+        it '302レスポンスが返る' do
+          expect(response.status).to eq 302
+        end
+        it 'データベースのユーザーが更新される' do
+          @user.reload
+          expect(@user.name).to eq 'new_name'
+        end
+        it 'users#showにリダイレクトする' do
+          expect(response).to redirect_to user_path(@user)
+        end
+      end
+      context '無効なパラメータの場合' do
+        before do
+          patch :update, params:{id: @user.id, user: attributes_for(:user, name: nil)}
+        end
+        it '200レスポンスが返る' do
+          expect(response.status).to eq 200
+        end
+        it 'データベースのユーザーは更新されない' do
+          @user.reload
+          expect(@user.name).to eq @originalname
+        end
+        it ':editテンプレートを再表示する' do
+          expect(response).to render_template :edit
+        end
+      end
+    end
+    context '被編集ユーザーとログインユーザーが一致しない' do
+      before do
+        @user, @login_user = create_list(:user, 2)
+        session[:user_id] = @login_user.id
+        patch :update, params:{id: @user.id, user: attributes_for(:user, name: "new_name")}
+      end
+      it "302レスポンスが返る" do
+        expect(response.status).to eq(302)
+      end
+      it 'rootにリダイレクトする' do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+  
+  describe "#favorite_articles" do
+    before do
+      @user = create(:user)
+      @article1, @article2, @article3 = create_list(:article, 3)
+      session[:user_id] = @user.id
+      create(:bookmark, user_id: @user.id, article_id: @article1.id)
+    end
+    context 'リクエストユーザーとログインユーザーが一致' do
+      before do
+        get :favorite_articles, params: {id: @user.id}
+      end
+      it "200レスポンスが返る" do
+        expect(response.status).to eq(200)
+      end
+      it "@userにリクエストされたユーザーを割り当てる" do
+        expect(assigns(:user)).to eq(@user)
+      end
+      it "@articleにリクエストされたユーザーがブックマークした記事を割り当てる" do
+        expect(assigns(:favorite_articles)).to eq([@article1])
+      end
+      it ':favorite_articlesテンプレートを表示する' do
+        expect(response).to render_template :favorite_articles
+      end
+    end
+    context 'リクエストユーザーとログインユーザーが一致しない' do
+      before do
+        @login_user = create(:user)
+        session[:user_id] = @login_user.id
+        get :favorite_articles, params: {id: @user.id}
+      end
+      it "302レスポンスが返る" do
+        expect(response.status).to eq(302)
+      end
+      it 'rootにリダイレクトする' do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
+
