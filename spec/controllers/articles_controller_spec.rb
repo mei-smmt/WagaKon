fd6ce77153cd6157cd49c7b71df36083c54e9d15
mei <jgtop1010@gmail.com>
2020-10-31 09:29:46 +0000

    articlesコントローラ #editテスト記述 #43

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -108,4 +108,38 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end
   end
+  
+  describe "#edit" do
+    context '記事作者とログインユーザーが一致' do
+      before do
+        @user = create(:user)
+        @article = create(:article, user_id: @user.id)
+        session[:user_id] = @user.id
+        get :edit, params: {id: @article.id}
+      end
+      it "200レスポンスが返る" do
+        expect(response.status).to eq(200)
+      end
+      it "@articleにリクエストされた記事を割り当てる" do
+        expect(assigns(:article)).to eq(@article)
+      end
+      it ':editテンプレートを表示する' do
+        expect(response).to render_template :edit
+      end
+    end
+    context '記事作者とログインユーザーが一致していない' do
+      before do
+        @user, @login_user = create_list(:user, 2)
+        @article = create(:article, user_id: @user.id)
+        session[:user_id] = @login_user.id
+        get :edit, params: {id: @article.id}
+      end
+      it "302レスポンスが返る" do
+        expect(response.status).to eq(302)
+      end
+      it 'rootにリダイレクトする' do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
