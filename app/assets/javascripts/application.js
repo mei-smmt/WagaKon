7d249fb6524962fb49d78f8551ddae1553794a50
mei <jgtop1010@gmail.com>
2020-12-09 11:49:00 +0000

    レシピ編集 プレビュー画像表示 #59

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,20 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// 画像プレビュー表示
+/* global $*/
+$( document ).on('turbolinks:load', function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#img_prev').attr('src', e.target.result);
+      }
+     reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#book_img").change(function(){
+    readURL(this);
+  });
+});
