1011f4fe6ad224400581ae2e0ed1cd1b3852cca6
mei <jgtop1010@gmail.com>
2020-12-09 06:07:46 +0000

    ブックマークの非同期化 #59

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,14 +2,14 @@ class BookmarksController < ApplicationController
 before_action :require_user_logged_in
 
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    current_user.bookmark(recipe)
-    redirect_to recipe
+    @recipe = Recipe.find(params[:recipe_id])
+    current_user.bookmark(@recipe)
+    # redirect_to recipe
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    current_user.unbookmark(recipe)
-    redirect_to recipe
+    @recipe = Recipe.find(params[:recipe_id])
+    current_user.unbookmark(@recipe)
+    # redirect_to recipe
   end
 end
