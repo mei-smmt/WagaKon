12db8208528fdce3100294381df8d296912dc959
mei <jgtop1010@gmail.com>
2020-12-11 06:39:50 +0000

    ingredients一括更新処理　修正 #54

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,17 +1,35 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+  has_one :ingredient_collection
   
   # 材料名必須
   validates :name, presence: true
 
-  # 材料の一括保存処理
-  def self.bulk_save(ingredients)
+  # 材料の一括更新処理
+  def self.bulk_update(recipe, new_ingredients)
     all_valid = true
-    Ingredient.transaction do
-      ingredients.each do |ingredient|
-        all_valid &= ingredient.save
+    # 登録したいレコード数が既存レコード数より多い場合、新規インスタンスを作成
+    diff = new_ingredients.size - recipe.ingredients.size
+    temp_id = Ingredient.last.id + 1
+    if diff > 0
+      new_ingredients.last(diff).each do |new_ingredient|
+        new_ingredient.merge!(id: temp_id)
+        temp_id += 1
+        recipe.ingredients.build(new_ingredient)
       end
-
+    end
+    # 以下、失敗したらロールバック
+    Ingredient.transaction do
+    　# 登録したいレコード数が既存レコード数より少ない場合、余分な既存レコードを削除
+      if diff < 0
+        recipe.ingredients.last(-diff).each do |ingredient|
+          ingredient.destroy
+        end
+      end
+      # 更新処理
+      recipe.ingredients.zip(new_ingredients) do |prev_ingredient, new_ingredient|
+        all_valid &= prev_ingredient.update(new_ingredient)
+      end
       unless all_valid
         raise ActiveRecord::Rollback
       end
