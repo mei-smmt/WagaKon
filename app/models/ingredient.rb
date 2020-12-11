492936079f9d47527226ee1ea305960e9ee7871b
mei <jgtop1010@gmail.com>
2020-12-11 11:38:55 +0000

    ingredients一括保存処理 修正 #54

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,31 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
-  has_one :ingredient_collection
-  
+
   # 材料名必須
   validates :name, presence: true
+
+  # 空フォーム除外
+  def self.remove_empty_form(new_ingredients)
+    new_ingredients.each do |new_ingredient|
+      if new_ingredient[:name].blank? && new_ingredient[:quantity].blank?
+        new_ingredients.delete(new_ingredient)
+      end
+    end
+  end
+
+  # 材料の一括保存処理
+  def self.bulk_save(ingredients)
+    all_valid = true
+    Ingredient.transaction do
+      ingredients.each do |ingredient|
+        all_valid &= ingredient.save
+      end
+      unless all_valid
+        raise ActiveRecord::Rollback
+      end
+    end
+    all_valid
+  end
 
   # 材料の一括更新処理
   def self.bulk_update(recipe, new_ingredients)
@@ -20,7 +42,7 @@ class Ingredient < ApplicationRecord
     end
     # 以下、失敗したらロールバック
     Ingredient.transaction do
-    　# 登録したいレコード数が既存レコード数より少ない場合、余分な既存レコードを削除
+    # 登録したいレコード数が既存レコード数より少ない場合、余分な既存レコードを削除
       if diff < 0
         recipe.ingredients.last(-diff).each do |ingredient|
           ingredient.destroy
