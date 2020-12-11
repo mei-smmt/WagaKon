ba6cdc63cbf754a5a8aecedb771bfec95543b935
mei <jgtop1010@gmail.com>
2020-12-11 06:39:50 +0000

    ingredients一括更新処理　修正 #54

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -34,25 +34,25 @@ class IngredientsController < ApplicationController
   end
   
   def update
-    @recipe.ingredients.destroy_all
-    @ingredients = []
-    ingredients = ingredients_params.is_a?(Array) ? ingredients_params : ingredients_params.values
-    ingredients.each do |ingredient|
-      if ingredient[:name].present? || ingredient[:quantity].present?
-        @ingredients << @recipe.ingredients.build(ingredient)
+    @ingredients = @recipe.ingredients
+    @new_ingredients = ingredients_params.is_a?(Array) ? ingredients_params : ingredients_params.values
+    # 空のフォームを除外
+    @new_ingredients.each do |new_ingredient|
+      if new_ingredient[:name].blank? && new_ingredient[:quantity].blank?
+        @new_ingredients.delete(new_ingredient)
       end
     end
-
-    if Ingredient.bulk_save(@ingredients)
+    # 一括更新処理呼び出し
+    if Ingredient.bulk_update(@recipe, @new_ingredients)
       redirect_to edit_recipe_steps_path(@recipe)
     else
       flash.now[:danger] = '内容に誤りがあります'
       render :edit
     end
   end
-
+  
   private
-    
+  
   def ingredients_params
     params.permit(ingredients: [:name, :quantity])["ingredients"]
   end
