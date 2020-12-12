4afef1c41fba655aa2eb0294c68a11a36c82909c
mei <jgtop1010@gmail.com>
2020-12-12 08:33:21 +0000

    一括保存、更新リファクタリング #54

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -14,13 +14,21 @@ class Ingredient < ApplicationRecord
   end
 
   # 材料の一括保存処理
-  def self.bulk_save(recipe, ingredients, new_ingredients)
+  def self.bulk_create(recipe, ingredients, form_ingredients)
+    # 空フォーム除外
+    new_ingredients = Ingredient.remove_empty_form(form_ingredients)
+    # 新規インスタンスを作成
+    new_ingredients.each do |new_ingredient|
+      ingredients << recipe.ingredients.build(new_ingredient)
+    end
     all_valid = true
+    # 以下、失敗したらロールバック
     Ingredient.transaction do
       ingredients.each do |ingredient|
         all_valid &= ingredient.save
       end
       unless all_valid
+      # render後のフォームを補充  
         missing_forms_size = 10 - new_ingredients.size
         missing_forms_size.times do
           ingredients << recipe.ingredients.build
@@ -32,11 +40,13 @@ class Ingredient < ApplicationRecord
   end
 
   # 材料の一括更新処理
-  def self.bulk_update(recipe, new_ingredients)
-    all_valid = true
+  def self.bulk_update(recipe, form_ingredients)
+    # 空フォーム除外
+    new_ingredients = Ingredient.remove_empty_form(form_ingredients)
+    # 仮idを設定
+    temp_id = Ingredient.last.id + 1
     # 登録したいレコード数が既存レコード数より多い場合、新規インスタンスを作成
     diff = new_ingredients.size - recipe.ingredients.size
-    temp_id = Ingredient.last.id + 1
     if diff > 0
       new_ingredients.last(diff).each do |new_ingredient|
         new_ingredient.merge!(id: temp_id)
@@ -44,9 +54,10 @@ class Ingredient < ApplicationRecord
         recipe.ingredients.build(new_ingredient)
       end
     end
+    all_valid = true
     # 以下、失敗したらロールバック
     Ingredient.transaction do
-    # 登録したいレコード数が既存レコード数より少ない場合、余分な既存レコードを削除
+      # 登録したいレコード数が既存レコード数より少ない場合、余分な既存レコードを削除
       if diff < 0
         recipe.ingredients.last(-diff).each do |ingredient|
           ingredient.destroy
@@ -57,6 +68,7 @@ class Ingredient < ApplicationRecord
         all_valid &= prev_ingredient.update(new_ingredient)
       end
       unless all_valid
+        # render後のフォームを補充  
         missing_forms_size = 10 - new_ingredients.size
         missing_forms_size.times do
           recipe.ingredients.build(id: temp_id)
