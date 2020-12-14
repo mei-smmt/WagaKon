25ba749aa408e985a186e5fa1dd562192a5048df
mei <jgtop1010@gmail.com>
2020-12-14 07:05:42 +0000

    バリデーション #66

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,9 @@
 class Meal < ApplicationRecord
+  validates :day_of_week, presence: true
+  
+  # day_of_weekカラム設定
+  enum day_of_week: { sun: 0, mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6 }
+  
+  belongs_to :user
+  has_many :munus
 end
