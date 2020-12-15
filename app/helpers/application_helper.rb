0a10556f1951cf82a9d40a9539b31c47da274fea
mei <jgtop1010@gmail.com>
2020-12-15 12:10:52 +0000

    helperメソッドの移管 #66

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def week_loop
+    week = []
+    day = Time.now.wday
+    7.times do
+      week << day
+      day = (day == 6) ? 0 : (day + 1)
+    end
+    week
+  end
+  
+  def wday_jp(index)
+    week = ["日", "月", "火", "水", "木", "金", "土"]
+    week[index]
+  end
 end
