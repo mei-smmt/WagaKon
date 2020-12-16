74026f024065bc48862b0795475605a892d6e9de
mei <jgtop1010@gmail.com>
2020-12-16 05:01:51 +0000

    features_table migrate #68

--- db/migrate/20201216045648_create_features.rb
+++ db/migrate/20201216045648_create_features.rb
@@ -1,7 +1,11 @@
 class CreateFeatures < ActiveRecord::Migration[5.2]
   def change
     create_table :features do |t|
-
+      t.integer :amount
+      t.integer :type
+      t.integer :cooking_method
+      t.integer :main_food
+      t.references :recipe, foreign_key: true
       t.timestamps
     end
   end
