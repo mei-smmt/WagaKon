8aa75988a4fc57e4df9dd1ae59dcf4ab7e8551c2
mei <jgtop1010@gmail.com>
2020-12-16 07:49:01 +0000

    enum日本語化設定 #68

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module DiyStock
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
   end
 end
