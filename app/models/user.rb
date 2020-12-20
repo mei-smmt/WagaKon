14a4fb2d4b1d226f2f39d303c8c8b4f3dd04c561
mei <jgtop1010@gmail.com>
2020-12-20 03:41:21 +0000

    personal_id バリデーション #71

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   before_save { self.email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :personal_id, presence: true, length: { in: 4..8 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 100 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-
   has_secure_password
-  validates :password, presence: true, length: { minimum: 4}
+  validates :password, presence: true, length: { in: 4..12 }
   
   has_many :recipes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
