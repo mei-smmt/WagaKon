734f1bc12438093d7458d9f823bc7c1ba02e20ac
mei <jgtop1010@gmail.com>
2020-12-27 09:31:55 +0000

    友だちのレシピのみアクセス可能にするバリデーション設定 #71

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,11 +1,7 @@
 class MealsController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   
   def index
     @meals = current_user.meals
-  end
-
-  def show
-    @meal = Meal.find(paramas[:id])
   end
 end
