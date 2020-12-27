4caf25a94ba321c3270381e4831cda0136fd74d3
mei <jgtop1010@gmail.com>
2020-12-27 14:10:27 +0000

    bookmarkコントローラ テスト関連修正 #74

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -5,53 +5,39 @@ RSpec.describe BookmarksController, type: :controller do
     before do
       include SessionsHelper
       @user = create(:user)
-      @recipe = create(:recipe)
+      @friend = create(:user)
+      @recipe = create(:recipe, status: "published", user_id: @friend.id)
+      create(:relationship, user_id: @user.id, friend_id: @friend.id, status: 'approved')
     end
     context "ログイン済み" do
       before do
         session[:user_id] = @user.id
       end
       context "同じ条件のブックマークが存在しない" do
-        it '302レスポンスが返る' do
-          post :create, params:{recipe_id: @recipe.id}
-          expect(response.status).to eq 302
-        end
         it 'データベースにユーザーの新しいブックマークが登録される' do
           expect{
-            post :create, params:{recipe_id: @recipe.id}
+            post :create, params:{recipe_id: @recipe.id}, xhr: true
           }.to change(@user.bookmarks, :count).by(1)
         end
         it 'データベースにレシピの新しいブックマークが登録される' do
           expect{
-            post :create, params:{recipe_id: @recipe.id}
+            post :create, params:{recipe_id: @recipe.id}, xhr: true
           }.to change(@recipe.bookmarks, :count).by(1)
-        end
-        it "レシピ詳細にリダイレクトする" do
-          post :create, params:{recipe_id: @recipe.id}
-          expect(response).to redirect_to recipe_path(@recipe)
         end
       end
       context "既に同じ条件のブックマークが存在する" do
         before do 
           create(:bookmark, user_id: @user.id, recipe_id: @recipe.id)
         end
-        it '302レスポンスが返る' do
-          post :create, params:{recipe_id: @recipe.id}
-          expect(response.status).to eq 302
-        end
         it 'データベースにユーザーの新しいブックマークが登録されない' do
           expect{
-            post :create, params:{recipe_id: @recipe.id}
+            post :create, params:{recipe_id: @recipe.id}, xhr: true
           }.not_to change(@user.bookmarks, :count)
         end
         it 'データベースにレシピの新しいブックマークが登録されない' do
           expect{
-            post :create, params:{recipe_id: @recipe.id}
+            post :create, params:{recipe_id: @recipe.id}, xhr: true
           }.not_to change(@recipe.bookmarks, :count)
-        end
-        it "レシピ詳細にリダイレクトする" do
-          post :create, params:{recipe_id: @recipe.id}
-          expect(response).to redirect_to recipe_path(@recipe)
         end
       end
     end
@@ -71,30 +57,24 @@ RSpec.describe BookmarksController, type: :controller do
     before do
       include SessionsHelper
       @user = create(:user)
-      @recipe = create(:recipe)
+      @friend = create(:user)
+      @recipe = create(:recipe, status: "published", user_id: @friend.id)
+      create(:relationship, user_id: @user.id, friend_id: @friend.id, status: 'approved')
       @bookmark = create(:bookmark, user_id: @user.id, recipe_id: @recipe.id)
     end
     context "ログイン済み" do
       before do
         session[:user_id] = @user.id
       end
-      it '302レスポンスが返る' do
-        delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}
-        expect(response.status).to eq 302
-      end
       it 'データベースからユーザーのブックマークが削除される' do
         expect{
-          delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}
+          delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}, xhr: true
         }.to change(@user.bookmarks, :count).by(-1)
       end
       it 'データベースからレシピのブックマークが削除される' do
         expect{
-          delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}
+          delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}, xhr: true
         }.to change(@recipe.bookmarks, :count).by(-1)
-      end
-      it "レシピ詳細にリダイレクトする" do
-        delete :destroy, params:{id: @bookmark.id, recipe_id: @recipe.id}
-        expect(response).to redirect_to recipe_path(@recipe)
       end
     end
     context "ログインなし" do
