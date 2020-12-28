604f012dad4dbd9148900b776bd50ad0c05d13bf
mei <jgtop1010@gmail.com>
2020-12-28 08:45:04 +0000

    デフォルト画像の設定 #80

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe < ApplicationRecord
 
   # タイトル、画像、説明文必須
   validates :title, presence: true, length: { maximum: 20 }
-  validates :image, presence: true
   validates :explanation, presence: true, length: { maximum: 400 }
   validates :status, presence: true
   
