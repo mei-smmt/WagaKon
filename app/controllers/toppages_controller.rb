9aa21a2666a8ddb6ad980a125841f134fc91738d
mei <jgtop1010@gmail.com>
2020-12-29 09:53:35 +0000

    検索ワード保持をsession利用に変更 #80

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,6 +2,14 @@ class ToppagesController < ApplicationController
   before_action :require_user_logged_in  
 
   def index
+    session_clear
     @recipes = current_user.accessable_recipes
+  end
+  
+  private
+  
+  def session_clear
+    session[:keyword].clear
+    session[:feature].clear
   end
 end
