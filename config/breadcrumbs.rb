7118e2c405916557efa48c4236f20d1a82082c71
mei <jgtop1010@gmail.com>
2021-01-12 09:53:48 +0000

    パンくずリスト作成 #80

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,25 +1,58 @@
 crumb :root do
-  link "Home", root_path
+  link "ホーム", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :mypage do
+  link "マイページ", user_path(current_user)
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :user_edit do
+  link "プロフィール編集", edit_users_path
+  parent :mypage
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :password_edit do
+  link "パスワード編集", password_edit_users_path
+  parent :user_edit
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
+crumb :favorite_recipes do
+  link "お気に入りレシピ", favorite_recipes_users_path
+end
+
+crumb :friends do
+  link "友だち", friends_users_path
+  parent :mypage
+end
+
+crumb :user do |user|
+  link "#{user.name}", user_path(user)
+  parent :friends
+end
+
+crumb :recipe do |recipe|
+  link "#{recipe.title}", recipe_path(recipe)
+end
+
+crumb :recipe_new do
+ link "レシピ概要の入力", new_recipes_path
+end
+
+crumb :recipe_edit do |recipe|
+  link "レシピ概要の入力", edit_recipe_path(recipe)
+  parent :recipe, recipe
+end
+
+crumb :ingredients_edit do |recipe|
+  link "材料の入力", edit_recipe_ingredients_path(recipe)
+  parent :recipe_edit, recipe
+end
+
+crumb :steps_edit do |recipe|
+  link "手順の入力", edit_recipe_steps_path(recipe)
+  parent :ingredients_edit, recipe
+end
+
 
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
