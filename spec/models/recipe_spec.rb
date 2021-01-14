6b771432601efe4125ce7de6bec0c6485f51d7c0
mei <jgtop1010@gmail.com>
2021-01-14 13:13:18 +0000

    recipeモデルテスト リファクタリング #97

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,28 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  before do
-    @recipe = build(:recipe)
-  end
-  
   describe 'バリデーション' do
-    it 'title, explanation, statusが入力されていればOK' do
-      expect(@recipe.valid?).to eq(true)
+    let(:recipe) { build(:recipe, params) }
+    context 'title, explanation, statusが入力されてる場合' do
+      let(:params) { { title: "チャーハン", explanation: "おいしいです。", status: "draft" } }
+      it 'OK' do
+        expect(recipe.valid?).to eq(true)
+      end
     end
-    
-    it 'titleが空だとNG' do
-      @recipe.title = nil
-      expect(@recipe.valid?).to eq(false)
+    context 'titleが空の場合' do
+      let(:params) { { title: nil } }
+      it 'NG' do
+        expect(recipe.valid?).to eq(false)
+      end
     end
-
-    it 'explanationが空だとNG' do
-      @recipe.explanation = nil
-      expect(@recipe.valid?).to eq(false)
+    context 'explanationが空の場合' do
+      let(:params) { { explanation: nil } }
+      it 'NG' do
+        expect(recipe.valid?).to eq(false)
+      end
     end
-    
-    it 'statusが空だとNG' do
-      @recipe.status = nil
-      expect(@recipe.valid?).to eq(false)
+    context 'statusが空の場合' do
+      let(:params) { { status: nil } }
+      it 'NG' do
+        expect(recipe.valid?).to eq(false)
+      end
     end
   end
 end
